8f0f7001310eefeb389215558b8cb66992d4d994
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-11-18 22:06:09 +0300

    fixing json

--- phase-2-w2-code-challenge/src/App.js
+++ phase-2-w2-code-challenge/src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from "react";
+//import BotCollection from './BotCollection';
 
 function App() {
-  fetch('')
+  const [botArmy, setBotArmy] = useState([]);
+  useEffect(()=> {
+    fetch('https://my-json-server.typicode.com/gabrielleoa/phase-2-w2-code-challenge')
+  .then((response) => response.json())
+  .then((data) => setBotArmy(data));
+}, []);
+
+const handleAddBots = (newBot) => {
+  setBotArmy([...botArmy, newBot]);
+};
+return (
+  <div>
+    <h1>
+      <h1 color='red'>Intergalactic Bot Army</h1>
+      {/*<BotCollection onAddBot={handleAddBots}/>*/}
+    </h1>
+  </div>
+)
+
 }
 
+  
 export default App;
